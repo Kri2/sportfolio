1cc6ef9709a6d40fbf73354efa94908ef3d803c4
Krzysztof Turlej <chris_peachtree@yahoo.com>
2017-07-07 12:04:49 +0200

    initial commit, empty project with one webapp module

---
+++ src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $io.github.kri2.sportfolio;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

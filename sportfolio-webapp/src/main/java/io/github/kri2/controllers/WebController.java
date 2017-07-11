7906a4e679be81b84870da677f7a4eae52c72fd9
Krzysztof Turlej <chris_peachtree@yahoo.com>
2017-07-11 15:36:39 +0200

    some refactoring

--- sportfolio-webapp/src/main/java/io/github/kri2/controllers/WebController.java
+++ sportfolio-webapp/src/main/java/io/github/kri2/controllers/WebController.java
@@ -6,6 +6,7 @@ import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -29,9 +30,14 @@ public class WebController {
 		
 		//easiest situation, standard json mapped directrly to a matching quote class
 		RestTemplate restTemplate = new RestTemplate();
-		Quote quote = restTemplate.getForObject("http://gturnquist-quoters.cfapps.io/api/random", Quote.class);
-        System.out.println(quote.toString());
-        model.addAttribute("quote_from_rest", quote.getValue().getQuote());
+		try{
+			Quote quote = restTemplate.getForObject("http://gturnquist-quoters.cfapps.io/api/random", Quote.class);
+			System.out.println(quote.toString());
+			model.addAttribute("quote_from_rest", quote.getValue().getQuote());
+		}catch(Exception e){
+			model.addAttribute("quote_from_rest", "not available");
+			System.out.println(e.getMessage());
+		}
         //that's it, enough if json is properly structured
         
 		//trying to do the same thing with google finance api
@@ -53,10 +59,16 @@ public class WebController {
         //Stock[] forNow = restTemplate.getForObject("http://finance.google.com/finance/info?client=ig&q=AAPL", Stock[].class);
         
 		//finally decided to to this way, not practical but proves the point -> get it as string, convert to json, mapp json to an object
-		String string = restTemplate.getForObject("http://finance.google.com/finance/info?client=ig&q=AAPL", String.class);
-        string=string.replace("//","").replace("[","").replace("]","").replace("\n","").replace("\t","");
-        System.out.println(string);//pytanie jak to ładnie przyporządkować do obiektu Stock teraz?
-        
+		String string="";
+		try{
+			string = restTemplate.getForObject("http://finance.google.com/finance/info?client=ig&q=AAPL", String.class);
+			string = string.replace("//","").replace("[","").replace("]","").replace("\n","").replace("\t","");
+			System.out.println(string);
+		}catch(Exception e){
+			System.out.println(e.getMessage());
+		}
+		
+         
         Gson gson = new Gson();
         gson.toJson(string);
         System.out.println(gson.toJson(string));
@@ -66,7 +78,9 @@ public class WebController {
         Stock stock1;
 		try {
 			stock1 = mapper.readValue(string, Stock.class);
-			System.out.println(stock1.getL_cur());
+			System.out.println(stock1.getPrice());
+			System.out.println(stock1.getTicker());
+			System.out.println(stock1.getCp());
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -91,3 +105,13 @@ public class WebController {
 		return "welcome";
 	}
 }
+
+
+/*
+ * Tak wygląda odpowiedz json
+ * {"type":"success","value":{"id":1,"quote":"Working with Spring Boot is like pair-programming with the Spring developers."}}
+ * tak "pseudo-json" z googla
+ * // [ { "id": "22144" ,"t" : "AAPL" ,"e" : "NASDAQ" ,"l" : "144.15" ,"l_fix" : "144.15" ,"l_cur" : "144.15" ,"s": "1" ,"ltt":"4:00PM EDT" ,"lt" : "Jul 7, 4:00PM EDT" ,"lt_dts" : "2017-07-07T16:00:05Z" ,"c" : "+1.42" ,"c_fix" : "1.42" ,"cp" : "0.99" ,"cp_fix" : "0.99" ,"ccol" : "chg" ,"pcls_fix" : "142.73" ,"el": "144.79" ,"el_fix": "144.79" ,"el_cur": "144.79" ,"elt" : "Jul 10, 6:10AM EDT" ,"ec" : "+0.64" ,"ec_fix" : "0.64" ,"ecp" : "0.44" ,"ecp_fix" : "0.44" ,"eccol" : "chg" ,"div" : "0.63" ,"yld" : "1.75" } ]
+ * 
+ */
+ 
--- sportfolio-webapp/src/main/java/io/github/kri2/domain/Stock.java
+++ sportfolio-webapp/src/main/java/io/github/kri2/domain/Stock.java
@@ -1,23 +1,34 @@
 package io.github.kri2.domain;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Stock {
-	private String t;
-	private String l_cur;
+	@JsonProperty("t")
+	private String ticker;
+	@JsonProperty("l_cur")
+	private String price;
+	private String cp;
+	public String getCp() {
+		return cp;
+	}
+	public void setCp(String cp) {
+		this.cp = cp;
+	}
 	public Stock(){
 	}
-	public String getT(){
-		return this.t;
+	public String getTicker(){
+		return this.ticker;
 	}
-	public void setT(String t){
-		this.t=t;
+	public void setTicker(String ticker){
+		this.ticker=ticker;
 	}
-	public String getL_cur(){
-		return this.l_cur;
+	public String getPrice(){
+		return this.price;
 	}
-	public void setL_cur(String l_cur){
-		this.l_cur=l_cur;
+	public void setPrice(String price){
+		this.price=price;
 	}
+	
 }
